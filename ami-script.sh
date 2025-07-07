19a554defaa64420296a5b49e1cb73cfafb25d4d
SaikrishnaG19 <saikrishna.g1719@gmail.com>
2025-07-07 14:56:50 +0530

    added package installation script

---
+++ ami-script.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# This script is used to create an Instance with docker, terraform and kubectl installed.
+
+sudo apt-get update -y
+sudo apt-get install curl wget unzip -y
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update   
+
+# Install Docker
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#verify dokcer installation
+docker --version
+
+#Install kubectl binary with curl on Linux 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" 
+#Download the kubectl checksum file: 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+#Validate the kubectl binary against the checksum file: 
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check  
+#install kubectl 
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl 
+#Verify kubectl installation
+kubectl version --client
+
+# Install Terraform
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common 
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+# Verify Terraform installation
+terraform -version
+
+#Install AWS CLI v2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+# Verify AWS CLI installation
+aws --version
